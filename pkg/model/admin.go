7dac50a7c54783a124d9ca13abb86a447f0d52fa
ratheeshkumar25 <ratheeshgopinadhkumar@gmail.com>
2024-12-02 23:34:33 +0530

    adminmodelupdated

--- pkg/model/admin.go
+++ pkg/model/admin.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Admin struct {
 	gorm.Model
-	Email    string  `gorm:"not null"`
-	Password string  `gorm:"not null"`
-	Wallet   float64 `gorm:"default:0"`
+	Email    string `gorm:"not null"`
+	Password string `gorm:"not null"`
+	//Wallet   float64 `gorm:"default:0"`
 }
